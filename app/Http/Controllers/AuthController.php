6353448a6fe555ea6f125eb7b8dcebefd943dec8
Joao Pedro <jp.ssilva@live.com>
2021-05-26 11:00:00 -0300

    Funcionalidade de login/logout e refresh

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,35 +12,79 @@ class AuthController extends Controller
     {
         $this->middleware('auth:api', [
             'except' => [
-                'login', 
-                'unauthorized', 
+                'login',
+                'unauthorized',
                 'create'
             ]
         ]);
     }
 
+    public function unauthorized()
+    {
+        return response()->json(['error' => 'Acesso não Autorizado!'], 401);
+    }
+
+    public function login(Request $request)
+    {
+        $array = ['error' => ''];
+
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+        if ($email && $password) {
+            $token = Auth::attempt([
+                'email' => $email,
+                'password' => $password
+            ]);
+
+            if (!$token) {
+                $array['error'] = 'E-mail e/ou Senha inválido(s)!';
+                return $array;
+            }
+
+            $array['token'] = $token;
+
+            return $array;
+        }
+
+        $array['error'] = 'Dados não enviados!';
+        return $array;
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return ['error' => ''];
+    }
+
+    public function refresh()
+    {
+        $token = Auth::refresh();
+        return [
+            'error' => '',
+            'token' => $token
+        ];
+    }
+
     public function create(Request $request)
     {
-        $array = ['error'=>''];
+        $array = ['error' => ''];
 
         $name = $request->input('name');
         $email = $request->input('email');
         $password = $request->input('password');
         $birthdate = $request->input('birthdate');
 
-        if($name && $email && $password && $birthdate)
-        {
+        if ($name && $email && $password && $birthdate) {
             //Validando a data
-            if(strtotime($birthdate)=== false)
-            {
-                $array['error'] = "Data de nascimento inválida!"; 
+            if (strtotime($birthdate) === false) {
+                $array['error'] = "Data de nascimento inválida!";
                 return $array;
             }
 
             //Verificando um e-mail existente
             $emailExists = User::where('email', $email)->count();
-            if($emailExists === 0)
-            {
+            if ($emailExists === 0) {
                 $hash = password_hash($password, PASSWORD_DEFAULT);
 
                 $newUser = new User();
@@ -49,32 +93,27 @@ class AuthController extends Controller
                 $newUser->password = $hash;
                 $newUser->birthdate = $birthdate;
                 $newUser->save();
-            
+
                 $token = Auth::attempt([
                     'email' => $email,
-                    'password' => $password 
+                    'password' => $password
                 ]);
 
-                if(!$token)
-                {
+                if (!$token) {
                     $array['error'] = "Ocorreu um erro!";
                     return $array;
                 }
 
                 $array['token'] = $token;
-            }
-            else
-            {
+            } else {
                 $array['error'] = "E-mail já cadastrado!";
                 return $array;
             }
-        }
-        else
-        {
+        } else {
             $array['error'] = "Alguns campos não foram enviados!";
             return $array;
         }
 
         return $array;
-    }//FIM DO CREATE
+    } //FIM DO CREATE
 }
--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return ['pong' => true];
 });
+Route::get('/401', 'App\Http\Controllers\AuthController@unauthorized')->name('login');
 
-// Route::get('/401', 'App\Http\Controllers\AuthController@unauthorized')->name('login');
+Route::post('/auth/login','App\Http\Controllers\AuthController@login');
+Route::post('/auth/logout','App\Http\Controllers\AuthController@logout');
+Route::post('/auth/refresh','App\Http\Controllers\AuthController@refresh');
 
-// Route::post('/auth/login','App\Http\Controllers\AuthController@login');
-// Route::post('/auth/logout','App\Http\Controllers\AuthController@logout');
-// Route::post('/auth/refresh','App\Http\Controllers\AuthController@refresh');
-
- Route::post('/user', 'App\Http\Controllers\AuthController@create');
+Route::post('/user', 'App\Http\Controllers\AuthController@create');
 // Route::put('/user', 'App\Http\Controllers\UserController@update');
 // Route::post('/user/avatar','App\Http\Controllers\UserController@updateAvatar');
 // Route::post('/user/cover','App\Http\Controllers\UserController@updateCover');

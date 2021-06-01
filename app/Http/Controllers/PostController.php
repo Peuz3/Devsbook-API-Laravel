c65849d432f9a8a5be76950788742bcf7b1a5dc3
Joao Pedro <jp.ssilva@live.com>
2021-06-01 10:50:50 -0300

    Comentando no POst

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\PostComment;
 use App\Models\PostLike;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -10,34 +11,35 @@ use Illuminate\Support\Facades\Auth;
 class PostController extends Controller
 {
     private $loggedUser;
-    
+
     public function __construct()
     {
         $this->middleware('auth:api');
         $this->loggedUser = auth()->user();
     }
 
-    public function like($id){
-        $array = ['error'=>''];
+    public function like($id)
+    {
+        $array = ['error' => ''];
 
         // Verificar se o post existe
         $postExists = Post::find($id);
-        if($postExists){
+        if ($postExists) {
             //Verficar se ja houve um like
             $isLiked = PostLike::where('id_post', $id)
-            ->where('id_user', $this->loggedUser['id'])
-            ->count();
+                ->where('id_user', $this->loggedUser['id'])
+                ->count();
 
-            if($isLiked >0 ){
+            if ($isLiked > 0) {
                 //Se sim, remover
                 $postLiked = PostLike::where('id_post', $id)
-                ->where('id_user', $this->loggedUser['id'])
-                ->first();
+                    ->where('id_user', $this->loggedUser['id'])
+                    ->first();
 
                 $postLiked->delete();
 
                 $array['isLiked'] = false;
-            }else{
+            } else {
                 //Se não, adicionar
                 $newPostLike = new PostLike();
                 $newPostLike->id_post = $id;
@@ -50,11 +52,38 @@ class PostController extends Controller
 
             $likeCount = PostLike::where('id_post', $id)->count();
             $array['likeCount'] = $likeCount;
-
-        }else{
+        } else {
             $array['error'] = "Post Inexistente!";
             return $array;
         }
+        return $array;
+    }
+
+    public function comment(Request $request, $id)
+    {
+        $array = ['error' => ''];
+
+        $txt = $request->input('txt');
+
+        $postExists = Post::find($id);
+
+        if ($postExists) {
+            if ($txt) {
+                $newPostComment = new PostComment();
+                $newPostComment->id_post = $id;
+                $newPostComment->id_user = $this->loggedUser['id'];
+                $newPostComment->created_at = date('Y-m-d H:i:s');
+                $newPostComment->body = $txt;
+                $newPostComment->save();
+            } else {
+                $array['error'] = 'Comentário não enviado!';
+                return $array;
+            }
+        } else {
+            $array['error'] = 'Post não existe!';
+            return $array;
+        }
+
         return $array;
     }
 }
